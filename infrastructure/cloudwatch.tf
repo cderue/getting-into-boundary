56739c89344d3e5cb1e7b19be3b69014321442f0
Cédric Derue <cedric.derue@gmail.com>
2024-10-05 19:57:28 +0000

    delete cloudwatch

--- infrastructure/cloudwatch.tf
+++
@@ -1,5 +0,0 @@
-# For logging with Fluent Bit
-#resource "aws_cloudwatch_log_group" "pod_logs" {
-#  name              = "/aws/eks/${aws_eks_cluster.cluster.name}/pod-logs"
-#  retention_in_days = 30
-#}
